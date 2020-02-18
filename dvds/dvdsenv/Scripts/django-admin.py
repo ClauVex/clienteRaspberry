bae28749a7b71f526b992bac24ec80be92fbbe5c
ClauVex <183383@ids.upchiapas.edu.mx>
2020-02-17 23:03:34 -0600

    raspberry1.0

---
+++ dvds/dvdsenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\claud\onedrive\escritorio\djangoraspberry2.0\dvds\dvdsenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
